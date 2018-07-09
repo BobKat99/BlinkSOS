a8d1c791279de220128082ff4e5b4a8ba7a516e2
BobKat99 <40940935+BobKat99@users.noreply.github.com>
2018-07-09 14:38:09 +1000

    Add files via upload

---
+++ BlinkSOS.ino
@@ -0,0 +1,41 @@
+void setup() {
+  pinMode (13, OUTPUT);
+}
+void loop() {
+  digitalWrite (13, HIGH);
+  delay (500);
+  digitalWrite (13, LOW);
+  delay (1000);
+  digitalWrite (13, HIGH),
+  delay (500);
+  digitalWrite (13, LOW),
+  delay (1000);
+  digitalWrite (13, HIGH),
+  delay (500);
+  digitalWrite (13, LOW),
+  delay (1000);
+  digitalWrite (13, HIGH);
+  delay (1500);
+  digitalWrite (13, LOW);
+  delay (1000);
+  digitalWrite (13, HIGH),
+  delay (1500);
+  digitalWrite (13, LOW),
+  delay (1000);
+  digitalWrite (13, HIGH),
+  delay (1500);
+  digitalWrite (13, LOW),
+  delay (1000);
+   digitalWrite (13, HIGH);
+  delay (500);
+  digitalWrite (13, LOW);
+  delay (1000);
+  digitalWrite (13, HIGH),
+  delay (500);
+  digitalWrite (13, LOW),
+  delay (1000);
+  digitalWrite (13, HIGH),
+  delay (500);
+  digitalWrite (13, LOW),
+  delay (1000);
+}
